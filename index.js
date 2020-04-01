61e6f783b0ff55556b1636b94ddf77781201486c
Serena Hasler <srhasler@yahoo.com>
2020-04-01 14:30:15 -0500

    HW due 4.02 Add function doubleValueLogger with comments.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//function declaration for tripler with one parameter
-function tripler(num) {
-  return num * 3; //function returns the result the number passed into the function times 3
+//declaration of function doubleValueLogger which has a 2 value parameter
+function doubleValueLogger(value1, value2) {
+  console.log(value1, value2); //prints the 2 values passed to the function
 }
-const shouldBeSix = tripler(2); //when used the variable will pass 2 to the function tripler
-console.log(shouldBeSix); //prints 6 -- the variable shouldBeSix passes 2 to the function tripler
+
+doubleValueLogger("Glorious", "Day!"); //prints Glorious Day! since these are the 2 values passed to doubleValueLogger
+doubleValueLogger("Greetings!"); //prints Greetings! undefined; Greetings! is the only value passed to doubleValueLogger. Since there are spaces for 2 parameters, the second space returns the value undefined.
