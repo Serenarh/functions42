aadc72008684b24e54efec81d7d10b609f6d27cc
Serena Hasler <srhasler@yahoo.com>
2020-04-01 14:03:32 -0500

    HW due 4.02 Add function greetingGenerator with comments.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-//start = beginning int; end = ending int; incr = incremented jump; all will be passed to the function numberSummer
-function numberSummer(start, end, incr) {
-  let total = 0;
-  for (let i = start; i <= end; i += incr) {
-    total += i;
-  }
-  return total;
+//a function DECLARATION called greetingGenerator
+function greetingGenerator() {
+  const phrase = "Good Morning!"; //local variable; not accessible globally
+  const p = "Hey!"; //local variable; not accessible globally
+  console.log(p); //will print "Hey!" b/c function accesses local variable value before looking outside for the global variable value
+  return phrase; //will return "Good Morning!"
 }
-console.log(numberSummer(0, 21, 3)); //start with 0, end at 21, count by 3s
+
+const p = "Hello!"; //global variable p has value "Hello!"
+console.log(p); //Hello! ; cannot go inside the scope of the function to find the local variable's value--uses global variable's value
+
+//console.log(phrase);
+
+const saying = greetingGenerator(); //Hey! the function is called and prints Hey! rather than Hello b/c the function first looks for a local value. It will only look outside its scope when no local value is available.absolute
+
+//saying(); //throws an error b/c saying is NOT a function--it is a string
+saying; //no output at all
